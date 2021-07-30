2640d5336becd01c7733e797d896173565752d2f
Alexandr Gorelov <gorelov2895@yandex.ru>
2021-07-30 17:59:33 +0300

    поправка теста для улучшения четабельности

--- test_add_group.py
+++ test_add_group.py
@@ -11,21 +11,15 @@ class UntitledTestCase(unittest.TestCase):
     
     def test_add_group(self):
         wd = self.wd
-        self.authorization_window_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="12345", header="12345", footer="12345"))
-        self.return_to_group_page(wd)
         self.logout(wd)
 
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.authorization_window_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_to_group_page(wd)
         self.logout(wd)
 
 
@@ -39,6 +33,7 @@ class UntitledTestCase(unittest.TestCase):
 
 
     def create_group(self, wd, group):
+        self.open_groups_page(wd)
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group firm
@@ -54,13 +49,14 @@ class UntitledTestCase(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
-
+        self.return_to_group_page(wd)
 
     def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
 
 
     def login(self, wd, username, password):
+        self.open_home_page(wd)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -71,7 +67,7 @@ class UntitledTestCase(unittest.TestCase):
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
 
-    def authorization_window_page(self, wd):
+    def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/group.php?selected%5B%5D=1&delete=Delete+group%28s%29")
 
     
