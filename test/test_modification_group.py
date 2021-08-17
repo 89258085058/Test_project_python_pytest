46de394e38aece37985c6a61222c00f44724338a
Alexandr Gorelov <gorelov2895@yandex.ru>
2021-08-17 08:13:05 +0300

    Удаление лишнего теста

--- test/test_modification_group.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from model.group import Group
-
-
-def test_modification_first_group(app):
-    app.session.login( username="admin", password="secret")
-    app.group.modification_first_group(Group(name="software", header="testing.", footer="ru"))
-    app.session.logout()
