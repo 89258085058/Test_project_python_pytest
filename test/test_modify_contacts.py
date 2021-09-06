ceb1f0979bb09f26d21b8886465d0bfe8a315678
Alexandr Gorelov <gorelov2895@yandex.ru>
2021-09-06 13:16:46 +0300

    Задание №20: Переделать тесты, чтобы сравнивались данные, загруженные из БД(1)

--- test/test_modify_contacts.py
+++ test/test_modify_contacts.py
@@ -5,15 +5,16 @@ import random
 
 def test_modify_contact_firstname_and_lastname(app, db, check_ui):
     if len(db.get_contact_list()) == 0:
-        app.contact.add_personal_information(Contact(firstname="1", lastname="1", homephone="1", mobilephone="1",
-                                                     workphone="1", secondaryphone="1", Address="1", email="1", email2="1", email3="1"))
-    old_cantacts = db.get_contact_list()
-    contact = random.choice(old_cantacts)
-    app.contact.modification_contact_by_id(contact.id, Contact(firstname="5", lastname="5", homephone="5", mobilephone="5",
-                   workphone="5", secondaryphone="5", Address="5", email="5", email2="5", email3="5"))
+        app.contact.add_personal_information(Contact(firstname="Alexandr", lastname="Gorelov"))
+    old_contacts = db.get_contact_list()
+    contact = random.choice(old_contacts)
+    index = old_contacts.index(contact)
+    new_contact = (Contact(firstname="TestName", lastname="TestLastName"))
+    app.contact.modification_contact_by_id(contact.id, new_contact)
+    assert len(old_contacts) == app.contact.count_contacts()
     new_contacts = db.get_contact_list()
-    assert sorted(old_cantacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+    old_contacts[index] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
     if check_ui:
         assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(),
                                                                      key=Contact.id_or_max)
-
