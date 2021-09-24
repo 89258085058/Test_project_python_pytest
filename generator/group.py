5b20fc20eb6d897e56754228c7919b201682b2d1
Alexandr Gorelov <gorelov2895@yandex.ru>
2021-09-24 20:29:27 +0300

    requirements

--- generator/group.py
+++ generator/group.py
@@ -24,7 +24,7 @@ for o, a in opt:
         f = a
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    symbols = string.ascii_letters
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 testdata = [Group(name="", header="", footer="")] + [
